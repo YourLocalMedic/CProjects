1309a590d9c389d8e3aa3077c448eda1d1ec662c
YourLocalMedic <breckinwallace@gmail.com>
2026-09-22 16:18:24 -0300

    add truefalse

---
+++ truefalse/truefalse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdbool.h>
+// jesus fucking christ you need to import true and false
+
+int main(){
+  bool x = true;
+  bool y = false;
+  printf("%b\n", x);
+  printf("%b\n", y);
+}
